982faff2bb93d0e6e75d3419bc1fb7acd2d831be
minhi0449 <minhi0449@gmail.com>
2025-04-07 23:04:17 +0900

    test: 상품 저장 테스트 메서드 createItemTest() 기본 구현

---
+++ src/test/java/com/SpringMall/repository/ItemRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.SpringMall.repository;
+
+import com.SpringMall.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+    }
+
+
+}
