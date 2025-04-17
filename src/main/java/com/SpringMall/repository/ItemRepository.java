dc33454fb6c23a0ebfde832b9f6e737e93d414c1
minhi0449 <minhi0449@gmail.com>
2025-04-17 17:28:59 +0900

    특정 가격 미만 상품 검색을 위한 LessThan 조건 메소드 추가

--- src/main/java/com/SpringMall/repository/ItemRepository.java
+++ src/main/java/com/SpringMall/repository/ItemRepository.java
@@ -17,6 +17,18 @@ import java.util.List;
     */
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    // 상품 조회하기
+    // itemNm(상품명)으로 데이터를 조회하기 위해서 By 뒤에 필드명인 ItemNm을 메소드의 이름에 붙여줌
+    // 엔티티명은 생략 가능하므로 findItemByItemNm 대신에 findByItemNm 으로 메소드명을 만들어줌
+    // 매개 변수로는 검색할 때 사용할 상품명 변수를 넘겨줌
     List<Item> findByItemNm(String itemNm);
+
+    // OR 조건 처리하기
+    // 상품을 상품명과 상품 상세 설명을 OR 조건을 이용하여 조회하는 쿼리 메소드
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    // LessThan 조건 처리하기
+    // 파라미터로 넘어온 price 변수보다 값이 작은 상품 데이터를 조회하는 쿼리 메소드
+    List<Item> findByPriceLessThan(Integer price);
 
 }
